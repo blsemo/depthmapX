1d702149531d69aa746573a4e1cf099250003dde
Christian Sailer <sar@mailraum.de>
2017-10-18 11:03:06 +0100

    Add attribute table view (holding current column and index to table)

--- salaTest/testattributetableview.cpp
+++ salaTest/testattributetableview.cpp
@@ -13,5 +13,29 @@ TEST_CASE("Test Attribute view"){
     AttributeTableView<SerialisedPixelRef> view(table);
     view.setDisplayColumn(0);
 
-    REQUIRE(view.getIndex().front().key.value == 7);
+    REQUIRE(view.getConstIndex().front().key.value == 7);
+}
+
+
+TEST_CASE("Test attribute table handle")
+{
+    using namespace  dXreimpl;
+    AttributeTable<SerialisedPixelRef> table;
+
+    table.insertOrResetColumn("foo");
+    table.insertOrResetColumn("bar");
+    table.addRow(0).setValue(0,1.0f).setValue(1, 1.1f);
+    table.addRow(7).setValue(0,0.7f).setValue(1,1.7f);
+
+    AttributeTableHandle<SerialisedPixelRef> handle(table);
+    handle.setDisplayColumn(0);
+
+    REQUIRE(handle.getIndex().front().key.value == 7);
+    REQUIRE(handle.getConstIndex().front().key.value == 7);
+
+
+    handle.getIndex().front().mutable_row->setValue(0, 0.8);
+
+    REQUIRE(table.getRow(7).getValue(0) == Approx(0.8));
+
 }
--- salalib/attributetableview.h
+++ salalib/attributetableview.h
@@ -13,11 +13,11 @@ public:
     const dXreimpl::AttributeTable<TKey> &m_table;
 
     // columnIndex < 0 -> not set
-    void setDisplayColumn(int columnIndex);
+    virtual void setDisplayColumn(int columnIndex);
     int getDisplayColumn() const{ return m_displayColumn;}
 
     typedef std::vector<dXreimpl::ConstAttributeIndexItem<TKey>> ConstIndex;
-    const ConstIndex& getIndex(){return m_index;}
+    const ConstIndex& getConstIndex() const{return m_index;}
 
 private:
     ConstIndex m_index;
@@ -41,5 +41,33 @@ template<typename TKey> void AttributeTableView<TKey>::setDisplayColumn(int colu
     m_index = dXreimpl::makeAttributeIndex<dXreimpl::ConstAttributeIndexItem<TKey>>(m_table, columnIndex);
     m_displayColumn = columnIndex;
 }
+
+template <typename TKey>
+class AttributeTableHandle : public AttributeTableView<TKey>
+{
+public:
+    AttributeTableHandle(dXreimpl::AttributeTable<TKey> &table) : m_mutableTable(table), AttributeTableView<TKey>(table){}
+    typedef std::vector<dXreimpl::AttributeIndexItem<TKey>> Index;
+    const Index& getIndex() const {return m_mutableIndex;}
+    virtual void setDisplayColumn(int columnIndex);
+private:
+    dXreimpl::AttributeTable<TKey>& m_mutableTable;
+    Index m_mutableIndex;
+
+};
+
+template<typename TKey> void AttributeTableHandle<TKey>::setDisplayColumn(int columnIndex){
+    if (columnIndex < 0)
+    {
+        m_mutableIndex.clear();
+    }
+    else
+    {
+        // recalculate the index even if it's the same column in case stuff has changed
+        m_mutableIndex = dXreimpl::makeAttributeIndex<dXreimpl::AttributeIndexItem<TKey>>(m_mutableTable, columnIndex);
+    }
+    AttributeTableView::setDisplayColumn(columnIndex);
+}
+
 
 #endif // ATTRIBUTETABLEVIEW_H
